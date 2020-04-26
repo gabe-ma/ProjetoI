e3afcdb4189ca194064f8813221e572ad5c385bd
gabe-ma <gabas9630@gmail.com>
2020-04-26 16:14:24 -0300

    atualização da lista de locais para coleta de lixo

--- src/dao/LocaisReciclagemDAO.java
+++ src/dao/LocaisReciclagemDAO.java
@@ -9,35 +9,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 import model.LocaisReciclagem;
-import model.TipoDeLixo;
-import controller.ListagemLocaisController;
 
 public class LocaisReciclagemDAO {
 	ArrayList<LocaisReciclagem> localLista;
 	LocaisReciclagem local;
 	
 	public List<LocaisReciclagem> carregar(int id) {
-		local = new LocaisReciclagem();
 		localLista = new ArrayList<LocaisReciclagem>();
 		String sqlSelect = "SELECT rua, numero, bairro, telefone, quantidade_lixo_toneladas FROM locais_reciclagem WHERE idlixos_reciclados = ?";
 		// usando o try with resources do Java 7, que fecha o que abriu
 		try (Connection conn = ConnectionFactory.obtemConexao();
 				PreparedStatement stm = conn.prepareStatement(sqlSelect);) {
-			stm.setInt(1, local.getIdLixoReciclado());
+			stm.setInt(1, id);
 			try (ResultSet rs = stm.executeQuery();) {
-				if (rs.next()) {
+				while (rs.next()) {
+					local = new LocaisReciclagem();
 					local.setRua(rs.getString("rua"));
 					local.setNumero(rs.getInt("numero"));
 					local.setBairro(rs.getString("bairro"));
 					local.setTelefone(rs.getString("telefone"));
 					local.setQuantLixo(rs.getInt("quantidade_lixo_toneladas"));
-					localLista.add(local);
-				} else {
-					local.setRua(null);
-					local.setNumero(-1);
-					local.setBairro(null);
-					local.setTelefone(null);
-					local.setQuantLixo(-1);
 					localLista.add(local);
 				}
 			} catch (SQLException e) {
